d62f008f2ce90eb6e2ce77caf3a4a978286f18f3
nice-gift <dmitry.luschick@yandex.by>
2023-10-17 15:07:54 +0300

    added JavaTimeModule to Json

--- src/main/java/by/it_academy/jd2/task_sql/service/factory/ObjectMapperFactory.java
+++ src/main/java/by/it_academy/jd2/task_sql/service/factory/ObjectMapperFactory.java
@@ -1,6 +1,8 @@
 package by.it_academy.jd2.task_sql.service.factory;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class ObjectMapperFactory {
     private static volatile ObjectMapper instance;
@@ -12,6 +14,8 @@ public class ObjectMapperFactory {
         if (instance == null) {
             synchronized (ObjectMapperFactory.class) {
                 instance = new ObjectMapper();
+                instance.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+                instance.registerModule(new JavaTimeModule());
             }
         }
         return instance;
